1d6a05cce2d9cf1198fc206a67847e86dd70a3a3
nikhilNathwani <njn27@cornell.edu>
2015-07-29 23:27:50 -0400

    In scrape.py, I implemented functions that enable me to see, for each NBA championship team, how many players on the roster started their career with that team. In util.py I have some useful utility functions e.g. for web scrapings and string parsing

---
+++ scrape.py
@@ -0,0 +1,73 @@
+from util import *
+
+def getChampions():
+	championListURL= "http://www.basketball-reference.com/leagues/"
+	#In the table of seasons, champion is the 3rd column
+	soup= grabSiteData(championListURL)
+	seasonRows= soup.find("tbody").findAll("tr")
+	champCells= [cols.findAll("td")[2] for cols in seasonRows]
+	champs= [champLink.find("a")["href"].encode("ascii","ignore") for champLink in champCells]
+	return champs
+
+def getPlayersOnTeam(teamLink):
+	teamSite= grabSiteData("http://www.basketball-reference.com"+teamLink)
+	#In the team's roster table, links to player pages are the 2nd column
+	rosterRows= teamSite.find("table",{"id" : "roster"}).find("tbody").findAll("tr")
+	rosterNames= [data.findAll("td")[1] for data in rosterRows]
+	playerLinks= [link.find('a')["href"].encode("ascii","ignore") for link in rosterNames]
+	return playerLinks
+
+def getPlayerDraftTeam(playerLink):
+	playerSite= grabSiteData("http://www.basketball-reference.com"+playerLink)
+	#first team played for is 3rd column of 1st row of stats table
+	totalStatsTable= playerSite.find("table",{"id":"totals"}).find("tbody")
+	rows= totalStatsTable.findAll("tr")[:2]
+
+	#if the player was traded in first year, first row is total for the year ("TOT")
+	firstTeam= rows[0].findAll("td")[2]
+	if firstTeam.findAll('a')==[] and firstTeam.text=="TOT":
+		firstTeam= rows[1].findAll("td")[2]
+
+	firstTeamName= firstTeam.find('a').text
+	return firstTeamName	
+
+
+def isSavior(playerLink,teamLink):
+	drafter= getPlayerDraftTeam(playerLink)
+	champ= teamFromURL(teamLink)
+	#print playerLink,drafter,champ
+	return drafter==champ
+
+def saviors(teamLink):
+	roster= getPlayersOnTeam(teamLink)
+	saviors= [player for player in roster if isSavior(player,teamLink)]
+	return saviors
+
+
+if __name__=="__main__":
+	'''
+	start= time.time()
+	champs= getChampions()
+	t1= time.time()
+	print "Time to get champions:", t1-start
+
+	roster= getPlayersOnTeam(champs[0])
+	t2= time.time()
+	print "Time to get roster:", t2-t1
+
+	drafter= getPlayerDraftTeam(roster[0])
+	t3= time.time()
+	print "Time to get drafter:", t3-t2
+	'''
+	start= time.time()
+	champs= getChampions()
+	t1= time.time()
+	print "Time to get champions:", t1-start
+
+	numSavs= []
+	for c in champs: 
+		s= saviors(c)
+		print c, len(s), s
+		numSavs.append(len(s))
+
+	print "Time to get num saviors", time.time()-t1
---
+++ util.py
@@ -0,0 +1,69 @@
+import numpy
+import sys
+import time
+import urllib2
+import csv	
+import os
+import json
+from bs4 import BeautifulSoup
+
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  web scraping  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+def grabSiteData(url):
+    usock= urllib2.urlopen(url)
+    data= usock.read()
+    usock.close()
+    return BeautifulSoup(data)
+
+
+
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  string manip  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+def urlToFilename(url):
+	elems= url[:-5].split("/")
+	team_name= elems[2]
+	yr= elems[3]
+	return team_name+'_'+yr+".csv"
+
+def yearFromURL(f):
+	return int(f[f.rfind('/')+1:f.find('.')])
+
+def teamFromURL(f):
+	stripOffYear= f[:f.rfind('/')]
+	year= stripOffYear[stripOffYear.rfind('/')+1:]
+	return year
+
+
+
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  file system  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+def getImmediateSubdirectories(dir):
+    return [name for name in os.listdir(dir)
+            if os.path.isdir(os.path.join(dir, name))]
+
+def getTeamYearsFromFolder(team):
+	currentDir= os.getcwd()
+	files= [name for name in os.listdir(currentDir+'/'+team)]
+	team_years= []
+	for f in files:
+		year= f[f.find('_')+1:f.find('.')]
+		team_years += [(f,year)]
+	return sorted(team_years, key=lambda tup: tup[1])
+            
+
+
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  csv read/write  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+def csvToLists(fn):
+	rows= []
+	with open(fn) as f:
+		for line in f:
+			arr= [elem.strip() for elem in line.split(',')]
+			rows.append(arr) 
+	return rows 
+
+def listsToCSV(lists,fn):
+	csv_file= open(fn,'w+')
+	csv_wr = csv.writer(csv_file)
+	for row in lists:
+		csv_wr.writerow(row)
